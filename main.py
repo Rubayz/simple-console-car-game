676de9f4527ff2b27deba63061649f9873e839f9
Rubayz1 <rubayzaf2007@gmail.com>
2021-03-26 15:10:35 +0000

    Literally finished half part of the work!

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world!")
+import random
+tries = 0
+name = input("Enter your name: ")
+name.strip()
+
+print(f"Hello {name}!")
+print("Would you like to play a game with me?")
+print("1)Yes")
+print("2)No")
+option = int(input("Select your option: "))
+if option == 1:
+	print("Car Game Starting!")
+elif option == 2:
+	print("Thank you!")
+else:
+	print("Invalid Option")
