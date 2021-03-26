f3d45e4f4c9279fde0a56c27df25993243583cc9
Rubayz1 <rubayzaf2007@gmail.com>
2021-03-26 15:21:47 +0000

    80% of the work is done!

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-tries = 0
+commands = ""
 name = input("Enter your name: ")
 name.strip()
 
@@ -10,6 +10,23 @@ print("2)No")
 option = int(input("Select your option: "))
 if option == 1:
 	print("Car Game Starting!")
+	print("There are some commands to start the car, to stop, to quit and the description of the commands is the help command!")
+	while True:
+		commands = input("> ")
+		if commands == "start":
+			print("Car is started.")
+		elif commands == "stop":
+			print("Car is stopped.")
+		elif commands == "help":
+			print("""
+start - To start the car.
+stop - To stop the car.
+quit - To quit.
+			""")
+		elif commands == "quit":
+			break
+		else:
+			print("Sorry I dont understand that!")
 elif option == 2:
 	print("Thank you!")
 else:
